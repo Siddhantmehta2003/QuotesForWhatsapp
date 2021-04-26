c3312b9ced6c3afb62b0bf29eb73593707d609a0
Siddhant Mehta <2010015323@adit.ac.in>
2021-04-26 23:07:57 +0530

    CallAPIandcollectdata

--- lib/Pages/SplashScreen.dart
+++ lib/Pages/SplashScreen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:mirage/Pages/homepage.dart';
 
@@ -9,12 +9,20 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  getJsonData() async {
+    var response = await http.get(
+        Uri.encodeFull(
+            "https://meme-api.herokuapp.com/gimme/ProgrammerHumor/10"),
+        headers: {"Accept": "application/json"});
+    print(response.body);
+  }
+
   @override
   void initState() {
     super.initState();
 
     //Currently simulating the background processes with a timer
-    //TODO: Add backgroud processes here
+    getJsonData();
     Timer _timer = Timer(Duration(seconds: 3), () {
       Navigator.of(context)
           .push(MaterialPageRoute(builder: (context) => HomePage()));
