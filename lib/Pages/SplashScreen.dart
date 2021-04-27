8ff45a8f27289691d56e1064e2cb515067b9ca15
Keval Prajapati <keval.prajapatimus@gmail.com>
2021-04-27 11:39:50 +0530

    Passed data to next screen

--- lib/Pages/SplashScreen.dart
+++ lib/Pages/SplashScreen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:mirage/Pages/homepage.dart';
@@ -9,11 +10,14 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  var data;
   getJsonData() async {
     var response = await http.get(
         Uri.encodeFull(
             "https://meme-api.herokuapp.com/gimme/ProgrammerHumor/10"),
         headers: {"Accept": "application/json"});
+    var convertDataToJson = json.decode(response.body);
+    data = convertDataToJson['memes'];
     print(response.body);
   }
 
@@ -25,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen> {
     getJsonData();
     Timer _timer = Timer(Duration(seconds: 3), () {
       Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => HomePage()));
+          .push(MaterialPageRoute(builder: (context) => HomePage(data: data)));
     });
   }
 
--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var data;
+
+  HomePage({@required this.data});
+  @override
+  _HomePageState createState() => _HomePageState(data);
+}
+
+class _HomePageState extends State<HomePage> {
+  var data;
+  _HomePageState(this.data);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffbdbdbd),
       body: Center(
-        child: Text(
-          "Homepage",
-          style: TextStyle(color: Colors.red, fontSize: 40),
-        ),
+        child: Image.network(data[0]["url"]),
       ),
     );
   }
